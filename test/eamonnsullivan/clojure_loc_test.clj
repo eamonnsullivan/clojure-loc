07d5a20d802b70c31cc4e2fdaaa615e63c2d2be3
Eamonn Sullivan <eamonn.sullivan@bbc.co.uk>
2020-01-05 20:48:02 +0000

    Didn't actually need a real file for the tests

--- test/eamonnsullivan/clojure_loc_test.clj
+++ test/eamonnsullivan/clojure_loc_test.clj
@@ -4,27 +4,17 @@
             [clojure.spec.test.alpha :as test]
             [eamonnsullivan.clojure-loc :refer :all]))
 
-(defn temp-file
-  [prefix suffix]
-  (doto (File/createTempFile prefix suffix)
-    (.deleteOnExit)))
-
 (deftest spec-tests
   (testing "non-blank?"
     (is (= 1 (:check-passed (test/summarize-results (test/check `non-blank?)))))))
 
 (deftest file-tests
   (testing "has-extension?"
-    (let [f (temp-file "something" ".xyz")]
-      (is (= true (has-extension? ".xyz" f)))
-      (is (= false (has-extension? ".txt" f)))))
+    (is (true? (has-extension? ".xyz" "test.xyz")))
+    (is (false? (has-extension? ".txt" "test.xyz"))))
   (testing "clojure-source?"
-    (let [f (temp-file "something" ".clj")
-          f2 (temp-file "something" ".else")]
-      (is (= true (clojure-source? f)))
-      (is (= false (clojure-source? f2)))))
+    (is (true? (clojure-source? "test.clj")))
+    (is (false? (clojure-source? "test.cljs"))))
   (testing "clojurescript-source?"
-    (let [f (temp-file "something" ".cljs")
-          f2 (temp-file "something" ".else")]
-      (is (= true (clojurescript-source? f)))
-      (is (= false (clojurescript-source? f2))))))
+    (is (true? (clojurescript-source? "test.cljs")))
+    (is (false? (clojurescript-source? "test.clj")))))
